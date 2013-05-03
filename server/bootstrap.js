479df49c085ef280cd097f6c06b643d3619755c3
timbrandin <tim.brandin@wunderkraut.com>
2013-05-03 11:45:10 +0200

    Fixed some errors during data import.

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -3,7 +3,7 @@ Meteor.startup(function () {
     var data = [
       {name: "Meteor Principles",
        desc: "Initial data for a story, example data.",
-       value: 4
+       value: 2
       },
       {name: "Principles",
        desc: "Initial data for a story, example data #2.",
@@ -11,8 +11,16 @@ Meteor.startup(function () {
       },
       {name: "Lipsum Principles",
        desc: "Initial data for a story, example data #3.",
-       value: 4
+       value: 1
       }
     ];
+    var timestamp = (new Date()).getTime();
+    for (var i = 0; i < data.length; i++) {
+      var stories_id = Stories.insert({
+        name: data[i].name,
+        desc: data[i].desc,
+        value: data[i].value
+      });
+    }
   }
 });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-Parties = new Meteor.Collection("stories");
+Stories = new Meteor.Collection("stories");
 
 Meteor.startup(function () {
   // code to run on server at startup
